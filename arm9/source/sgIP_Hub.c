5333e50140698db91bf1a76df027cca0fe9190bd
Dave Murphy <wntrmute@users.sourceforge.net>
2007-02-05 09:18:18 +0000

    allow broadcast on 255.255.255.255

--- arm9/source/sgIP_Hub.c
+++ arm9/source/sgIP_Hub.c
@@ -145,7 +145,10 @@ int sgIP_Hub_SendProtocolPacket(int protocol, sgIP_memblock * packet, unsigned l
 		return 0;
 	}
 	// resolve protocol address to hardware address & send packet
-	if((src_address & hw->snmask) == (dest_address & hw->snmask)) { // on same network, send directly.
+ if((src_address & hw->snmask) == (dest_address & hw->snmask) || (dest_address | hw->snmask) == 0xFFFFFFFF )
+	if( (src_address & hw->snmask) == (dest_address & hw->snmask)	// on same network
+	 	|| (dest_address | hw->snmask) == 0xFFFFFFFF )				// or broadcast address, send directly.
+	{ 
 		return sgIP_ARP_SendProtocolFrame(hw,packet,protocol,dest_address);
 	} else { // eek, on different network. Send to gateway
 		return sgIP_ARP_SendProtocolFrame(hw,packet,protocol,hw->gateway);
