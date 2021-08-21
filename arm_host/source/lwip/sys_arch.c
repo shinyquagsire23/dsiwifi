0e03f79a395f9ce49f78a7f2e01aef82fb7448ef
shinyquagsire23 <mtinc2@gmail.com>
2021-08-21 16:46:32 -0700

    Make all lwip threading primitives weak funcs on the off chance someone brings their own threading

--- arm_host/source/lwip/sys_arch.c
+++ arm_host/source/lwip/sys_arch.c
@@ -8,7 +8,7 @@
 
 int errno;
 
-void sys_init(void)
+void __attribute__((weak)) sys_init(void)
 {
 }
 
@@ -38,66 +38,66 @@ sys_now(void)
 #define MBOX_VAL_EMPTY (0x12345678)
 #define MBOX_VAL_INVALID (0x12345679)
 
-err_t sys_mutex_new(sys_mutex_t *mutex)
+err_t __attribute__((weak)) sys_mutex_new(sys_mutex_t *mutex)
 {
     *mutex = 0xFFFFFFFF;
     return ERR_OK;
 }
 
-void sys_mutex_lock(sys_mutex_t *mutex)
+void __attribute__((weak)) sys_mutex_lock(sys_mutex_t *mutex)
 {
 }
 
-void sys_mutex_unlock(sys_mutex_t *mutex)
+void __attribute__((weak)) sys_mutex_unlock(sys_mutex_t *mutex)
 {
 }
 
-void sys_mutex_free(sys_mutex_t *mutex)
+void __attribute__((weak)) sys_mutex_free(sys_mutex_t *mutex)
 {
     *mutex = 0;
 }
 
-int sys_mutex_valid(sys_mutex_t *mutex)
+int __attribute__((weak)) sys_mutex_valid(sys_mutex_t *mutex)
 {
     return !!(mutex);
 }
 
-void sys_mutex_set_invalid(sys_mutex_t *mutex)
+void __attribute__((weak)) sys_mutex_set_invalid(sys_mutex_t *mutex)
 {
     
 }
 
-err_t sys_sem_new(sys_sem_t *sem, u8_t count)
+err_t __attribute__((weak)) sys_sem_new(sys_sem_t *sem, u8_t count)
 {
     *sem = 0xFFFFFFFF;
     return ERR_OK;
 }
 
-void sys_sem_signal(sys_sem_t *sem)
+void __attribute__((weak)) sys_sem_signal(sys_sem_t *sem)
 {
     
 }
 
-u32_t sys_arch_sem_wait(sys_sem_t *sem, u32_t timeout)
+u32_t __attribute__((weak)) sys_arch_sem_wait(sys_sem_t *sem, u32_t timeout)
 {
     return 1;
 }
 
-void sys_sem_free(sys_sem_t *sem)
+void __attribute__((weak)) sys_sem_free(sys_sem_t *sem)
 {
     *sem = 0;
 }
 
-int sys_sem_valid(sys_sem_t *sem)
+int __attribute__((weak)) sys_sem_valid(sys_sem_t *sem)
 {
     return !!(sem);
 }
 
-void sys_sem_set_invalid(sys_sem_t *sem)
+void __attribute__((weak)) sys_sem_set_invalid(sys_sem_t *sem)
 {
 }
 
-void sys_msleep(u32_t ms)
+void __attribute__((weak)) sys_msleep(u32_t ms)
 {
     vu32 target = val + ms;
     
@@ -107,7 +107,7 @@ void sys_msleep(u32_t ms)
     while (val < target) ;
 }
 
-err_t sys_mbox_new(sys_mbox_t *mbox, int size)
+err_t __attribute__((weak)) sys_mbox_new(sys_mbox_t *mbox, int size)
 {
     if (!mbox) return ERR_MEM;
     
@@ -115,7 +115,7 @@ err_t sys_mbox_new(sys_mbox_t *mbox, int size)
     return ERR_OK;
 }
 
-void sys_mbox_post(sys_mbox_t *mbox, void *msg)
+void __attribute__((weak)) sys_mbox_post(sys_mbox_t *mbox, void *msg)
 {
     if (!mbox || *mbox == MBOX_VAL_INVALID) return;
 
@@ -133,7 +133,7 @@ void sys_mbox_post(sys_mbox_t *mbox, void *msg)
     *mbox = (uint32_t)msg;
 }
 
-err_t sys_mbox_trypost(sys_mbox_t *mbox, void *msg)
+err_t __attribute__((weak)) sys_mbox_trypost(sys_mbox_t *mbox, void *msg)
 {
     if (!mbox || *mbox == MBOX_VAL_INVALID) return ERR_MEM;
     
@@ -154,7 +154,7 @@ err_t sys_mbox_trypost(sys_mbox_t *mbox, void *msg)
     return ERR_OK;
 }
 
-err_t sys_mbox_trypost_fromisr(sys_mbox_t *mbox, void *msg)
+err_t __attribute__((weak)) sys_mbox_trypost_fromisr(sys_mbox_t *mbox, void *msg)
 {
     if (!mbox || *mbox == MBOX_VAL_INVALID) return ERR_MEM;
 
@@ -174,7 +174,7 @@ err_t sys_mbox_trypost_fromisr(sys_mbox_t *mbox, void *msg)
     return ERR_OK;
 }
 
-u32_t sys_arch_mbox_fetch(sys_mbox_t *mbox, void **msg, u32_t timeout)
+u32_t __attribute__((weak)) sys_arch_mbox_fetch(sys_mbox_t *mbox, void **msg, u32_t timeout)
 {
     if (!mbox || *mbox == MBOX_VAL_INVALID) return SYS_ARCH_TIMEOUT;
     *msg = NULL;
@@ -207,7 +207,7 @@ u32_t sys_arch_mbox_fetch(sys_mbox_t *mbox, void **msg, u32_t timeout)
     return 0;
 }
 
-u32_t sys_arch_mbox_tryfetch(sys_mbox_t *mbox, void **msg)
+u32_t __attribute__((weak)) sys_arch_mbox_tryfetch(sys_mbox_t *mbox, void **msg)
 {
     if (!mbox || *mbox == MBOX_VAL_INVALID) return SYS_MBOX_EMPTY;
     
@@ -223,25 +223,25 @@ u32_t sys_arch_mbox_tryfetch(sys_mbox_t *mbox, void **msg)
     return 0;
 }
 
-void sys_mbox_free(sys_mbox_t *mbox)
+void __attribute__((weak)) sys_mbox_free(sys_mbox_t *mbox)
 {
     if (!mbox) return;
     *mbox = MBOX_VAL_INVALID;
 }
 
-int sys_mbox_valid(sys_mbox_t *mbox)
+int __attribute__((weak)) sys_mbox_valid(sys_mbox_t *mbox)
 {
     if (!mbox) return 0;
     
     return (*mbox != MBOX_VAL_INVALID);
 }
 
-void sys_mbox_set_invalid(sys_mbox_t *mbox)
+void __attribute__((weak)) sys_mbox_set_invalid(sys_mbox_t *mbox)
 {
     *mbox = MBOX_VAL_INVALID;
 }
 
-sys_thread_t sys_thread_new(const char *name, lwip_thread_fn thread, void *arg, int stacksize, int prio)
+sys_thread_t __attribute__((weak)) sys_thread_new(const char *name, lwip_thread_fn thread, void *arg, int stacksize, int prio)
 {
     return 0;
 }
